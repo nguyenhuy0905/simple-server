16177d13dd0bc824176ef916c9ecf9cea75ec135
Huy Nguyen <huy55465@gmail.com>
2025-04-20 10:28:09 -0400

    wip(printf): organize labels, begin sprintf
    Commit-by: Huy Nguyen

--- printf.asm
+++ printf.asm
@@ -39,6 +39,16 @@ CARRIAGE = 13
 ; \r -> carriage
 ; %d -> number on va_arg list
 printf:
+  ; Some macro to refer to memory positions more easily
+  label .t_fd qword at rbp-8
+  label .t_buf qword at rbp-16
+  label .t_buflen qword at rbp-24
+  label .va_arg_ptr qword at rbp-32
+  label .retval qword at rbp-40
+  label .strbuf_idx qword at rbp-48
+  ; buf_idx equ qword [rbp-56]
+  label .buf_idx qword at rbp-56
+
   push rbp
   mov rbp, rsp
   ; rsp at this point:
@@ -51,35 +61,27 @@ printf:
   ; imma just use rax, then rdi, rsi and rdx as temporary registers.
   sub rsp, 64
   ; push all the arguments passed in to the stack
-  mov qword [rbp-8], rdi
-  mov qword [rbp-16], rsi
-  mov qword [rbp-24], rdx
+  mov [.t_fd], rdi
+  mov [.t_buf], rsi
+  mov [.t_buflen], rdx
   ; local variables
   lea rax, [rbp+16]
-  mov qword [rbp-32], rax
-  mov qword [rbp-40], 0
-  ; Some macro to refer to these memory positions more easily
-  t_fd equ qword [rbp-8]
-  t_buf equ qword [rbp-16]
-  t_buflen equ qword [rbp-24]
-  va_arg_ptr equ qword [rbp-32]
-  retval equ qword [rbp-40]
+  mov [.va_arg_ptr], rax
+  mov [.retval], 0
 
   ; strbuf index. Only meaningful in the context of .loop_put_str.
-  mov qword [rbp-48], 0
-  strbuf_idx equ qword [rbp-48]
+  mov [.strbuf_idx], 0
   
   ; format buffer index. Only meaningful in the context of .loop_put_str.
-  mov qword [rbp-56], 0
-  buf_idx equ qword [rbp-56]
+  mov [.buf_idx], 0
 .loop_put_str:
   ; while (r9 < rdx)
-  mov r9, buf_idx
-  mov rdx, t_buflen
+  mov r9, [.buf_idx]
+  mov rdx, [.t_buflen]
   cmp r9, rdx
   je .end_loop_put_str
   ; while-body
-  mov rsi, t_buf
+  mov rsi, [.t_buf]
   cmp byte [rsi+r9], "\"
   je .match_backslash
   cmp byte [rsi+r9], "%"
@@ -88,26 +90,26 @@ printf:
   
 .begin_put_str:
   c_to_put equ cl
-  mov rsi, t_buf
+  mov rsi, [.t_buf]
   mov c_to_put, byte [rsi+r9]
-  mov rax, strbuf_idx
+  mov rax, [.strbuf_idx]
   mov byte [strbuf+rax], c_to_put
-  inc strbuf_idx
-  inc retval
+  inc [.strbuf_idx]
+  inc [.retval]
   c_to_put equ
 .end_put_str:
-  inc buf_idx
+  inc [.buf_idx]
   jmp .loop_put_str
 
-; modifies r10 to be the position to write to.
+; modifies r8 to be the position to write to.
 macro bound_check label_if_ok, label_if_done {
   ; paranoia
-  mov rsi, t_buf
-  mov r9, buf_idx
-  mov rdx, t_buflen
+  mov rsi, [.t_buf]
+  mov r9, [.buf_idx]
+  mov rdx, [.t_buflen]
   ; comparison
-  lea r10, [rsi+r9+1]
-  cmp r10, [rsi+rdx]
+  lea r8, [rsi+r9+1]
+  cmp r8, [rsi+rdx]
   ; if the backslash is the last char, do nothing.
   ; otherwise, look to the next char:
   ; - if next char is t, put 9 to strbuf.
@@ -116,29 +118,29 @@ macro bound_check label_if_ok, label_if_done {
   jmp label_if_done
 }
 .match_backslash:
-  ; at this point, r9, rsi and rdx should be loaded with buf_idx, t_buf and
-  ; t_buflen respectively
+  ; at this point, r9, rsi and rdx should be loaded with buf_idx, .t_buf and
+  ; .t_buflen respectively
   bound_check .match_backslash_peek_next, .end_match
 .match_backslash_peek_next:
-  ; use r10 here
-  macro r10_match_char char_cmp, jmp_label {
-    cmp byte [r10], char_cmp
+  ; use r8 here
+  macro r8_match_char char_cmp, jmp_label {
+    cmp byte [r8], char_cmp
     je jmp_label
   }
-  r10_match_char "t", .match_backslash_put_tab
-  r10_match_char "n", .match_backslash_put_newline
-  r10_match_char "\", .match_backslash_put_backslash
-  r10_match_char "%", .match_backslash_put_percent
-  r10_match_char "r", .match_backslash_put_carriage
+  r8_match_char "t", .match_backslash_put_tab
+  r8_match_char "n", .match_backslash_put_newline
+  r8_match_char "\", .match_backslash_put_backslash
+  r8_match_char "%", .match_backslash_put_percent
+  r8_match_char "r", .match_backslash_put_carriage
   jmp .begin_put_str
 
   ; only useful in the context of match_backslash really
   macro put_char to_put {
-    mov rax, strbuf_idx
+    mov rax, [.strbuf_idx]
     mov byte [strbuf+rax], to_put
-    inc buf_idx
-    inc strbuf_idx
-    inc retval
+    inc [.buf_idx]
+    inc [.strbuf_idx]
+    inc [.retval]
     jmp .end_put_str
   }
 .match_backslash_put_tab:
@@ -151,38 +153,76 @@ macro bound_check label_if_ok, label_if_done {
   put_char "%"
 .match_backslash_put_carriage:
   put_char CARRIAGE
+  
+  purge put_char
 
 .match_percent:
   ; TODO: match_percent
   bound_check .match_percent_peek_next, .end_match
 .match_percent_peek_next:
-  r10_match_char "d", .match_percent_put_int
+  r8_match_char "d", .match_percent_put_int
   jmp .begin_put_str
 .match_percent_put_int:
   ; prepare to call the function
-  mov t_buf, rsi
+  mov [.t_buf], rsi
   ; get next arg in va_list 
-  mov rax, va_arg_ptr
+  mov rax, [.va_arg_ptr]
   mov edi, dword [rax]
   ; I'm lazy.
   ; technically I can save space by only pushing 4 bytes on here.
-  add va_arg_ptr, 8
-  mov rax, strbuf_idx
+  add [.va_arg_ptr], 8
+  mov rax, [.strbuf_idx]
   lea rsi, [strbuf+rax]
   mov rdx, strbuf.len
-  sub rdx, strbuf_idx
+  sub rdx, [.strbuf_idx]
   call int32_to_str
-  mov rsi, t_buf
-  add retval, rax
-  add strbuf_idx, rax
-  inc buf_idx
+  mov rsi, [.t_buf]
+  add [.retval], rax
+  add [.strbuf_idx], rax
+  inc [.buf_idx]
   jmp .end_put_str
 
+  purge bound_check
 .end_loop_put_str:
 
   ; printing it out
-  write t_fd,strbuf,retval
-  mov rax, retval
+  write [.t_fd],strbuf,[.retval]
+  mov rax, [.retval]
+.cleanup:
+  leave
+  ret
+
+; args:
+; rdi: the buffer to put stuff to.
+; rsi: the max length of the put-stuff buffer.
+; rdx: the address of the format buffer.
+; rcx: the length to format.
+; return through rax the number of bytes printed, or -1 if error.
+;
+; va_args are passed like so:
+; HIGHEST MEMORY ADDRESS
+; last argument
+; ...
+; second argument
+; first argument
+; LOWEST MEMORY ADDRESS
+;
+; note, this is NOT how you pass va_args in C.
+; here, we still have 5 more registers to pass in va_args.
+; until we use up all registers, we move on to pushing onto stack.
+; for now, if you pass in too few va_arg, there's no bound-checking mechanism.
+;
+; supported parse symbols:
+; \n -> newline
+; \t -> tab
+; \\ -> backslash
+; \% -> percent
+; \r -> carriage
+; %d -> number on va_arg list
+sprintf:
+  ; save stack frame
+  push rbp
+  mov rbp, rsp
 .cleanup:
   leave
   ret
