692b6169ac47e6238e59ab8e7ed56717e033ae82
Huy Nguyen <huy55465@gmail.com>
2025-04-20 17:01:12 -0400

    feat(capi): constexpr

--- capi.c
+++ capi.c
@@ -5,17 +5,17 @@ extern size_t asm_printf(int t_fd, const char *t_addr, size_t t_len, ...);
 extern size_t asm_sprintf(char *restrict t_put_buf, size_t t_put_buf_len,
                           const char *restrict t_fmt_buf, size_t t_fmt_buf_len,
                           ...);
-static const char str_to_print[] = "Hello\t %d/%d/%d\n";
-static const size_t strsize = sizeof(str_to_print);
+constexpr char str_to_print[] = "Hello\t %d/%d/%d\n";
+constexpr size_t strsize = sizeof(str_to_print);
 static char fmt_buf[1024] = {0};
-static const size_t fmt_buf_size = sizeof(fmt_buf);
+constexpr size_t fmt_buf_size = sizeof(fmt_buf);
 
 [[noreturn]] void nolibc_exit(int retcode) {
   __asm__("mov $60, %%rax\n\t"
           "mov %[retcode], %%rdi\n\t"
           "syscall\n\t"
           :
-          : [retcode] "r"((size_t)retcode)
+          : [retcode] "r"((int64_t)retcode)
           : "rax");
   unreachable();
 }
