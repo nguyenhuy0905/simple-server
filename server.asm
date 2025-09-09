dccbc427ad78c548095bbfcfa33392f0c3ed709f
Huy Nguyen <139140486+nguyenhuy0905@users.noreply.github.com>
2025-09-09 13:25:35 -0400

    feat: listen log, accept macro
    Time to move to client i suppose
    
    Signed-off-by: Huy Nguyen <139140486+nguyenhuy0905@users.noreply.github.com>

--- server.asm
+++ server.asm
@@ -10,13 +10,10 @@ public _start
 _start:
   extrn asm_printf
   label sock qword at rbp-8
+  label newsock qword at rbp-16
 
   mov rbp, rsp
-  sub rsp, 8
-  ; mov rdi, STDOUT
-  ; mov rsi, open_sock_log_msg
-  ; mov rdx, open_sock_log_msg.len
-  ; call asm_printf
+  sub rsp, 16
   call_printf STDOUT, open_sock_log_msg, open_sock_log_msg.len
   ; This is a manual call compared to the nice macro call_printf
   xor rax, rax
@@ -59,7 +56,26 @@ _start:
 @@:
 
   listen [sock], 1
+  cmp rax, -1
+  jne @f
+  mov rdi, STDOUT
+  mov rsi, listen_sock_err_msg
+  mov rdx, listen_sock_err_msg.len
+  call asm_printf
+  mov r12, -1
+  jmp .cleanup
+@@:
+  mov rdi, STDOUT
+  mov rsi, listen_sock_success_msg
+  mov rdx, listen_sock_success_msg.len
+  mov rcx, [sock]
+  call asm_printf
+  accept [sock], 0, 0
+  cmp rax, -1
+  jne @f
 
+@@:
+  mov [newsock], rax
   xor r12, r12
 .cleanup:
   cmp [sock], -1
@@ -80,6 +96,9 @@ open_sock_err_msg string "Error opening socket (return %d)!!!\n"
 bind_sock_err_msg string "Error binding socket %d!\nstrace to trace!\n"
 server_sockaddr sockaddr_in AF_INET, 6969, 127, 0, 0, 1
 sockaddr_fmt string ".sin_port = %d\n.s_addr = %d.%d.%d.%d\n"
+listen_sock_err_msg string "Error listening socket %d\n"
+listen_sock_success_msg string "Listening on socket %d\n"
+accept_sock_err_msg string "Error accepting socket\n"
 
 section '.bss' writeable
 strbuf arr 1024
